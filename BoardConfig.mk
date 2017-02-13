83a76e373f8ff2dd3b49a881244f0d894828dd9a
Mansiu3000 <bjsiu0617@gmail.com>
2017-02-13 18:43:27 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@
 
 
 # Platform
-TARGET_BOARD_PLATFORM := mt6582
+TARGET_BOARD_PLATFORM := mt6589
 TARGET_NO_BOOTLOADER := true
 
 # Architecture
@@ -28,14 +28,14 @@ TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a7
 
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := mt6582
+TARGET_BOOTLOADER_BOARD_NAME := mt6589
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK := device/gionee/m2/MTKbootimg.mk
+BOARD_CUSTOM_BOOTIMG_MK := device/xiaomi/HM2013023/MTKbootimg.mk
 BOARD_CUSTOM_BOOTIMG := true
-TARGET_PREBUILT_KERNEL := device/gionee/m2/kernel
+TARGET_PREBUILT_KERNEL := device/xiaomi/HM2013023/kernel
 TARGET_KMODULES := true
 BOARD_KERNEL_CMDLINE := 
 BOARD_MKBOOTIMG_ARGS := --base 0x10000000 --pagesize 2048 --kernel_offset 0x00008000 --second_offset 0x00f00000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
@@ -59,22 +59,21 @@ ADDITIONAL_DEFAULT_PROPERTIES += ro.mount.fs=EXT4
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 # TWRP
-TARGET_RECOVERY_FSTAB := device/gionee/m2/twrp.fstab
+TARGET_RECOVERY_FSTAB := device/xiaomi/HM2013023/twrp.fstab
 TW_THEME := portrait_hdpi
-TARGET_SCREEN_HEIGHT := 854
-TARGET_SCREEN_WIDTH := 480
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
 TW_INCLUDE_CRYPTO := true
 BOARD_HAS_LARGE_FILESYSTEM := true
-TW_NO_USB_STORAGE := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_NO_REBOOT_BOOTLOADER := true
 TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
 TW_MAX_BRIGHTNESS := 255
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
-TW_INTERNAL_STORAGE_PATH := "/sdcard1"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "Phone Storage"
-TW_EXTERNAL_STORAGE_PATH := "/sdcard0"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "Micro SDCard"
+TW_INTERNAL_STORAGE_PATH := "/sdcard0"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "Internal Storage"
+TW_EXTERNAL_STORAGE_PATH := "/sdcard1"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "External SDCard"
 
 # Usb
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun%d/file"
